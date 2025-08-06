a2bfa8a77c4d28e2bea04e956dd5a21cb69ba7bb
Dimas <dmlitvinov99@gmail.com>
2025-08-06 15:10:38 -0400

    New CorsConfig

--- src/main/java/com/example/backend_videostore/config/CorsConfig.java
+++ src/main/java/com/example/backend_videostore/config/CorsConfig.java
@@ -15,9 +15,8 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://front-videostore.vercel.app"));
-
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedOriginPatterns(List.of("https://front-videostore.vercel.app"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
